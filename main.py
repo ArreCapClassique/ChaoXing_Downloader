affca033f936823b3ac2a6e45cc86ab5c28da9dc
BCccc64 <57453310+BCccc64@users.noreply.github.com>
2022-03-12 19:25:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,41 @@
 from selenium import webdriver
-import time,os,re,chaojiying
+import time,os,random
 
-def captcha_discern():
-    client = chaojiying.Chaojiying_Client('','','')     #依次输入超级鹰平台的 用户名，密码，软件ID
-    with open('captcha.png', 'rb') as f:
-        image = f.read()
-        captcha = client.PostPic(image,1902)['pic_str']
-    print('captcha discerned: '+captcha)
-    return captcha
 
-def login():
-    username = driver.find_element_by_id('unameId')
-    password = driver.find_element_by_id('passwordId')
-    numcode = driver.find_element_by_id('numcode')
-    button = driver.find_element_by_class_name('zl_btn_right')
-    numVercode = driver.find_element_by_xpath('//*/img')
-    numVercode.screenshot('captcha.png')
-    username.send_keys('')                           #输入超星平台用户名
-    password.send_keys('')                           #输入密码
-    numcode.send_keys(captcha_discern())
-    os.remove('captcha.png')
-    time.sleep(1)
-    button.click()
+class chaoxing:
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+        self.domain = 'https://i.chaoxing.com/'
+        options = webdriver.ChromeOptions()
+        #options.add_argument('--headless')
+        options.add_argument('log-level=3')
+        options.add_experimental_option("detach", True)
+        options.add_experimental_option('prefs', {'download.default_directory': os.path.realpath(__file__)[:-7]+'Downloads'})   #下载路径
+        self.driver = webdriver.Chrome(executable_path=os.path.dirname(__file__)+'/chromedriver.exe',options=options)
+        self.driver.implicitly_wait(10)
 
-def downloader():
-    if 'studentstudy' in link:
-        driver.switch_to.frame('iframe')
-        li = driver.find_elements_by_xpath('//*/iframe')
-    elif 'coursedata' in link:
-        li = driver.find_elements_by_xpath('//*/tr')
-    li = {i.get_attribute('objectid') for i in li}
-    for i in li :
-        if i:
-            driver.get('http://d0.ananas.chaoxing.com/download/'+i)
-            print('successfully downloaded '+'(ID: '+i+')')
+    def login(self):
+        self.driver.get(self.domain)
+        self.driver.find_element_by_xpath('//*[@id="phone"]').send_keys(self.username)
+        self.driver.find_element_by_xpath('//*[@id="pwd"]').send_keys(self.password)
+        self.driver.find_element_by_xpath('//*[@id="loginBtn"]').click()
+        time.sleep(1)
+
+    def crawl(self):
+        link = input("Plz paste the link here: ")
+        self.driver.get(link)
+        self.driver.switch_to.frame(0)
+        li = self.driver.find_elements_by_xpath('//*/iframe')
+        li = {i.get_attribute('objectid') for i in li}
+        for i in li:
+            self.driver.get('https://cs-ans.chaoxing.com/download/' + i)
             time.sleep(1)
 
+    def run(self):
+        self.login()
+        self.crawl()
+
+
 if __name__ == "__main__":
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option('prefs', {'download.default_directory': os.path.realpath(__file__)[:-7]+'Downloads'})   #下载路径
-    link = input('Plz paste the link here: ')
-    driver = webdriver.Chrome(executable_path=os.path.realpath(__file__)[:-7]+'chromedriver.exe',chrome_options=options)
-    driver.get(link)
-    time.sleep(1)
-    login()
-    print('successfully broke in')
-    downloader()
+    chaoxing("","").run()
